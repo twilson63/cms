6afc398a0d8eab31664e46d1e85859e5a9a5477b
Tom Wilson <tom@jackhq.com>
2016-11-05 13:08:01 -0400

    refactored api

--- api/index.js
+++ api/index.js
@@ -3,30 +3,38 @@ const PouchDB = require('pouchdb-http')
 PouchDB.plugin(require('pouchdb-mapreduce'))
 PouchDB.plugin(require('pouchdb-upsert'))
 
-const { composeP, prop, pluck, head } = require('ramda')
+const { composeP, prop, pluck, head,
+  ifElse, isEmpty } = require('ramda')
+
 const db = PouchDB(process.env.DB)
+
+module.exports = async function (req, res) {
+  // GET /profiles_page
+  return await composeP(
+    ifElse(isEmpty, _ => ({ok: false}), head),
+    pluck('value'),
+    prop('rows'),
+    url =>
+      db.query('doc', {key: url, limit: 1})
+        .catch(err => ({ok: false}))
+  )(req.url)
+}
+
+// setup view if does not exist
 db.putIfNotExists({
   _id: '_design/doc',
   language: 'javascript',
   views: {
-    key: {
+    doc: {
       map: function (doc) {
-        doc.items.forEach(function (item) {
-          emit('/' + doc.name + '/' + item.key, item.data)
-        })
+        if (doc.type === 'content') {
+          var v = {}
+          doc.items.forEach(function (item) {
+            v[item.key] = item.data
+          })
+          emit('/' + doc.name, v)
+        }
       }.toString()
     }
   }
 }).catch(err => console.log(err))
-
-module.exports = async function (req, res) {
-  // GET /profiles_page/title
-  return await composeP(
-    head,
-    pluck('value'),
-    prop('rows'),
-    url => db.query('doc/key', {key: url, limit: 1})
-  )(req.url)
-
-  //return {ok: true}
-}
